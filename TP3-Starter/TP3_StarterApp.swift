e357f8dd8533cad58bc525af5e3a49bbc0db93bd
oualid kiram <oualid.kiram1@gmail.com>
2025-04-16 14:17:49 -0400

    Initial Commit

---
+++ TP3-Starter/TP3_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TP3_StarterApp.swift
+//  TP3-Starter
+//
+//  Created by Oualid Kiram on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct TP3_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TP3-StarterTests/TP3_StarterTests.swift
@@ -0,0 +1,16 @@
+//
+//  TP3_StarterTests.swift
+//  TP3-StarterTests
+//
+//  Created by Oualid Kiram on 2025-04-16.
+//
+
+import Testing
+
+struct TP3_StarterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
